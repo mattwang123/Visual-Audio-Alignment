56b6f7cf4aa2a1c94816ea4ef77acf8b7f8e706a
Hexuan Wang <hwang302@jh.edu>
2025-05-09 20:34:25 -0400

    revise feature

--- get_feature/run_extraction.py
+++ get_feature/run_extraction.py
@@ -2,6 +2,6 @@ from feature_extraction import batch_process
 
 batch_process(
     dataset_dir=r"D:\lenovo\mia_final_project\generated_samples",
-    output_dir=r"D:\lenovo\mia_final_project\Visual-Audio-Alignment\preprocessed_output",
-    max_workers=6
+    output_dir=r"D:\lenovo\mia_final_project\preprocessed_output",
+    max_workers=12
 )
